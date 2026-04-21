da36c7157812f97aece4f467f090e2367ae4380f
Ezzaldeen-Albitar <ezzaldeenalbitar9@gmail.com>
2026-04-21 12:46:14 +0300

    fix bug

--- BackEnd/utils/sendEmail.js
+++ BackEnd/utils/sendEmail.js
@@ -1,26 +1,30 @@
+// utils/sendEmail.js
 const nodemailer = require("nodemailer");
+require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
-});
-
-const sendEmail = async (to, subject, htmlContent) => {
+const sendEmail = async (options) => { 
     try {
-        const info = await transporter.sendMail({
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
+
+        const mailOptions = {
             from: `"ShopNow" <${process.env.SMTP_USER}>`,
-            to: options.email,
+            to: options.email,   
             subject: options.subject,
             html: options.html,
-        });
-        console.log("Message sent: %s", info.messageId);
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully: " + info.messageId);
         return info;
-    } catch (err) {
-        console.error("Error while sending mail:", err);
-        throw err;
+    } catch (error) {
+        console.error("Error in sendEmail utility:", error.message);
+        throw error;
     }
 };
 
